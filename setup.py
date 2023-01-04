36cd8b758d43914eabcba307c5894d97444ec773
Sibo Wang <sibo.wang@epfl.ch>
2023-01-04 14:44:57 +0100

    implement downloading utils

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    requirements = [l for l in requirements if not l.strip().startswith('#')]
+
+setuptools.setup(
+    name='fancpq',
+    version='0.0.1',
+    description='Prioritizing segments to proofread in FANC',
+    author='Sibo Wang',
+    author_email='sibo.wang@epfl.ch',
+    packages=setuptools.find_packages(),
+    install_requires=requirements
+)
