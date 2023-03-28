6fce72cfab328b19686d907262eaf8e2fedab10c
Sibo Wang <sibo.wang@epfl.ch>
2023-03-28 12:27:41 +0200

    Add CAVE tables and update setup.py to copy the data
    These tables are out of date but will be rematerialized upon verison
    update

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import setuptools
+import pkg_resources
+import shutil
+from pathlib import Path
+
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -11,5 +15,17 @@ setuptools.setup(
     author='Sibo Wang',
     author_email='sibo.wang@epfl.ch',
     packages=setuptools.find_packages(),
-    install_requires=requirements
+    install_requires=requirements,
+    package_data={'ysp_bot': ['config.yaml', 'data/*']},
+    include_package_data=True
 )
+
+
+import yaml
+config_path = Path(pkg_resources.resource_filename('ysp_bot', 'config.yaml'))
+data_path = Path(pkg_resources.resource_filename('ysp_bot', 'data'))
+with open(config_path) as f:
+    dump_dir = Path(yaml.safe_load(f)['local']['data']).expanduser() / 'dump'
+dump_dir.mkdir(parents=True, exist_ok=True)
+shutil.copytree(data_path / 'cave_506', dump_dir/ 'cave_506',
+                dirs_exist_ok=True)
